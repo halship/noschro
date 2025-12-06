bc0584c24088495d5ff3c5a04f78f241d2cd46e5
halship <spship132@gmail.com>
2025-12-06 17:06:47 +0900

    画像のリンクテキストは別の表示をするように変更

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -33,21 +33,35 @@
                 // URL文字列の場合
                 const url: string = urlResult[0];
                 const urlBase: string = urlResult[1];
+                const imgResult = url.match(/^.+\.(jpg|jpeg|gif|png|webp|mp4)/i);
 
-                result.push('<a href="');
-                result.push(url);
-                result.push('" target="_blank" class="underline">');
-
-                if (urlBase.length > 100) {
-                    result.push(urlBase.slice(0, 100 - 3));
-                    result.push('...');
+                if (imgResult) {
+                    const imgType = imgResult[1].toLowerCase();
+                    // 画像の場合
+                    result.push('<a href="');
+                    result.push(url);
+                    result.push('" target="_blank" class="underline">');
+                    result.push('[Open media (type: ' + imgType + ')]');
+                    result.push('</a>');
+                    
+                    i += url.length;
                 } else {
-                    result.push(urlBase);
-                }
+                    // 外部リンクの場合
+                    result.push('<a href="');
+                    result.push(url);
+                    result.push('" target="_blank" class="underline">');
 
-                result.push('</a>');
-                
-                i += url.length;
+                    if (urlBase.length > 100) {
+                        result.push(urlBase.slice(0, 100 - 3));
+                        result.push('...');
+                    } else {
+                        result.push(urlBase);
+                    }
+
+                    result.push('</a>');
+                    
+                    i += url.length;
+                }
             } else if (content.slice(i).startsWith('\n')) {
                 // 改行の場合
                 result.push('<br>');
