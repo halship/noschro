6065a497fc78a650733add4f1e0b6911a51f4eee
halship <spship132@gmail.com>
2025-12-06 16:46:16 +0900

    ダークモードを実装

---
+++ src/lib/ThemeButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { Sun, Moon } from '@lucide/svelte';
+    import { browser } from '$app/environment';
+
+    let theme = 'light';
+
+    if (browser) {
+        theme = localStorage.getItem('theme') ?? 'light';
+        document.documentElement.classList.toggle('dark', localStorage.getItem('theme') === 'dark');
+    }
+
+    function toggleTheme() {
+        if (browser) {
+            if (theme === 'light') {
+                theme = 'dark';
+            } else {
+                theme = 'light';
+            }
+
+            localStorage.setItem('theme', theme);
+            document.documentElement.classList.toggle('dark');
+        }
+    }
+</script>
+
+<button class="border border-thin rounded-full p-1" onclick={toggleTheme}>
+    {#if theme === 'light'}
+        <Sun />
+    {:else}
+        <Moon />
+    {/if}
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
 	import './layout.css';
-	import { Sun, House } from '@lucide/svelte';
+	import { House } from '@lucide/svelte';
+	import ThemeButton from '$lib/ThemeButton.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<title>ノスクロ -noschro-</title>
+	<title>ノスクロ</title>
 </svelte:head>
 
-<header class="bg-light border-thin text-dark border-b p-1 inset-x-0 top-0 sticky flex">
+<header class="bg-light dark:bg-dark border-thin text-dark dark:text-light border-b p-1 inset-x-0 top-0 sticky flex">
 	<ul class="flex-auto flex items-center pl-1">
 		<li class="mr-2">
 			<a href="/" class="text-lg"><House /></a>
 		</li>
 	</ul>
 	<div class="pr-1">
-		<button class="border border-thin rounded-full p-1"><Sun /></button>
+		<ThemeButton />
 	</div>
 </header>
 
-<div id="main" class="bg-light text-dark max-w-3xl mx-auto p-2">
+<div id="main" class="bg-light dark:bg-dark text-dark dark:text-light max-w-3xl mx-auto p-2">
 	{@render children()}
 </div>
