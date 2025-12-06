8e19f19bf39ee1dcf3ce9324d0f75a47bcc7d098
halship <spship132@gmail.com>
2025-12-06 15:42:58 +0900

    Homeリンクをアイコンに変更＆テーマ変更ボタンをガワだけ追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import './layout.css';
+	import { Sun, House } from '@lucide/svelte';
 
 	let { children } = $props();
 </script>
@@ -8,10 +9,15 @@
 	<title>ノスクロ -noschro-</title>
 </svelte:head>
 
-<header class="bg-light border-thin text-dark border-b p-1 inset-x-0 top-0 sticky">
-	<h1 class="text-3xl">
-		<a href="/">ノスクロ</a>
-	</h1>
+<header class="bg-light border-thin text-dark border-b p-1 inset-x-0 top-0 sticky flex">
+	<ul class="flex-auto flex items-center pl-1">
+		<li class="mr-2">
+			<a href="/" class="text-lg"><House /></a>
+		</li>
+	</ul>
+	<div class="pr-1">
+		<button class="border border-thin rounded-full p-1"><Sun /></button>
+	</div>
 </header>
 
 <div id="main" class="bg-light text-dark max-w-3xl mx-auto p-2">
