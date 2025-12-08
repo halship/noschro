f73ed8d5681ada3e904799a18a3c31a45e1c2112
halship <spship132@gmail.com>
2025-12-08 12:39:01 +0900

    プロフィールページの概要も投稿と同じようにフォーマットするよう修正

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import DOMPurify from "isomorphic-dompurify";
 	import type { NostrEvent, NostrProfile, NostrRef } from "$lib/types/nostr";
 	import { neventEncode, npubEncode } from "nostr-tools/nip19";
+    import { formatContent } from "$lib/util";
 
     export let event: NostrEvent;
     export let profile: NostrProfile | null;
@@ -10,80 +10,6 @@
     // 整数で表現された日時を表示用に整形する
     function formatTime(ts: number) {
         return new Date(ts * 1000).toLocaleString();
-    }
-
-    // イベント内容を表示用に整形する
-    function formatContent(content: string) {
-        content = DOMPurify.sanitize(content);
-
-        const result: string[] = [];
-        let i: number = 0
-
-        while (i < content.length) {
-            const urlResult = content.slice(i).match(/^https?:\/\/([\w!?/+\-=_~;.,*&@#$%()'[\]]+)/);
-            const codeResult = content.slice(i).match(/^nostr:[a-z0-9]+/);
-            
-            if (urlResult) {
-                // URL文字列の場合
-                const url: string = urlResult[0];
-                const urlBase: string = urlResult[1];
-                const imgResult = url.match(/^.+\.(jpg|jpeg|gif|png|webp|mp4)/i);
-
-                if (imgResult) {
-                    const imgType = imgResult[1].toLowerCase();
-                    // 画像の場合
-                    result.push('<a href="');
-                    result.push(url);
-                    result.push('" target="_blank" class="underline">');
-                    result.push('[Open media (type: ' + imgType + ')]');
-                    result.push('</a>');
-                    
-                    i += url.length;
-                } else {
-                    // 外部リンクの場合
-                    result.push('<a href="');
-                    result.push(url);
-                    result.push('" target="_blank" class="underline">');
-
-                    if (urlBase.length > 100) {
-                        result.push(urlBase.slice(0, 100 - 3));
-                        result.push('...');
-                    } else {
-                        result.push(urlBase);
-                    }
-
-                    result.push('</a>');
-                    
-                    i += url.length;
-                }
-            } else if (content.slice(i).startsWith('\n')) {
-                // 改行の場合
-                result.push('<br>');
-                i++;
-            } else if (codeResult) {
-                const code = codeResult[0].slice(6);
-
-                if (code.startsWith('nevent')) {
-                    result.push('<a href="/');
-                    result.push(code);
-                    result.push('" class="underline">[引用]</a>');
-                } else if (code.startsWith('note')) {
-                    result.push('<a href="/');
-                    result.push(code);
-                    result.push('" class="underline">[引用]</a>');
-                } else {
-                    result.push(codeResult[0]);
-                }
-
-                i += codeResult[0].length;
-            } else {
-                // その他の場合
-                result.push(content.slice(i).charAt(0));
-                i++;
-            }
-        }
-
-        return result.join('');
     }
 
     function getEventCode(ev: NostrEvent): string {
--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
     import DOMPurify from "isomorphic-dompurify";
-	import type { NostrProfile } from "./types/nostr";
+	import type { NostrProfile } from "$lib/types/nostr";
+    import { formatContent } from "$lib/util";
 
     export let profile: NostrProfile;
-
-    function formatAbout(about: string | undefined): string {
-        if (!about) return '';
-        return DOMPurify.sanitize(about).replaceAll('\n', '<br>');
-    }
 </script>
 
 <div class="profile border-thin border rounded-md p-2 mt-2">
     <div class="user-display-name font-bold text-lg wrap-break-word">{profile.display_name}</div>
     <div class="user-name text-thin wrap-break-word">@{profile.name}</div>
-    <div class="user-about mt-5 wrap-break-word">{@html formatAbout(profile.about)}</div>
+    <div class="user-about mt-5 wrap-break-word">{@html formatContent(profile.about ?? '')}</div>
 </div>
