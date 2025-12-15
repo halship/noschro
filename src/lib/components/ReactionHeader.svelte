5a9383bbb5501741692a0c53e5dfc6bac2c2d3ee
halship <spship132@gmail.com>
2025-12-15 14:39:42 +0900

    レイアウト修正

--- src/lib/components/ReactionHeader.svelte
+++ src/lib/components/ReactionHeader.svelte
@@ -25,10 +25,10 @@
 			{@html formatReaction(event.content, event.tags)}
 		{/if}
 	</span>
-	<span
-		class="reaction-display-name font-bold grow-0 shrink basis-auto min-w-0 whitespace-nowrap overflow-hidden mr-1"
-	>
-		<a href="/{npubEncode(event.pubkey)}">
+	<a href="/{npubEncode(event.pubkey)}">
+		<span
+			class="reaction-display-name font-bold grow-0 shrink basis-auto min-w-0 whitespace-nowrap overflow-hidden mr-1"
+		>
 			{#if profile?.display_name}
 				{@html formatDisplayName(profile?.display_name!, profile?.tags)}
 			{:else if profile?.name}
@@ -36,12 +36,14 @@
 			{:else}
 				{event.pubkey.substring(0, 9)}
 			{/if}
-		</a>
-	</span>
+		</span>
 
-	<span class="reaction-name text-thin grow shrink min-w-0 whitespace-nowrap overflow-hidden mr-1">
-		{#if profile?.display_name && profile?.name && profile?.display_name !== profile?.name}
-			@{profile?.name!}
-		{/if}
-	</span>
+		<span
+			class="reaction-name text-thin grow shrink min-w-0 whitespace-nowrap overflow-hidden mr-1"
+		>
+			{#if profile?.display_name && profile?.name && profile?.display_name !== profile?.name}
+				@{profile?.name!}
+			{/if}
+		</span>
+	</a>
 </div>
