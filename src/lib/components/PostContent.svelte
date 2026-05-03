d5debc4bd4931927855239e6073f2b314ed4a34a
halship <spship132@gmail.com>
2026-05-04 08:34:17 +0900

    添付画像をリンクで開けるように

--- src/lib/components/PostContent.svelte
+++ src/lib/components/PostContent.svelte
@@ -14,9 +14,11 @@
 		{#if token.type === 'text'}
 			{token.text}
 		{:else if token.type === 'link'}
-			<a href={token.url} class="underline">{formatLink(token.url)}</a>
+			<a href={token.url} rel="external" class="underline">{formatLink(token.url)}</a>
 		{:else if token.type === 'image'}
-			<img src={token.url} class="h-80 rounded-md" alt="content" />
+			<a href={token.url} rel="external"
+				><img src={token.url} class="h-80 rounded-md border border-gray-600" alt="content" /></a
+			>
 		{/if}
 	{/each}
 </div>
