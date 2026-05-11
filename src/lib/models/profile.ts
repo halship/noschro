437ef562f050d63891f7fe72533d0108a5ebea23
halship <spship132@gmail.com>
2026-05-12 08:27:31 +0900

    Remove app state dependency from profile model

--- src/lib/models/profile.ts
+++ src/lib/models/profile.ts
@@ -1,7 +1,6 @@
 import type { NostrEvent } from '$lib/nostr_type';
 import type * as Nostr from 'nostr-typedef';
 import { parseName, type NostrToken } from './token';
-import { appState } from '$lib/state.svelte';
 import { extractContentPubkeys, extractQuoteIds } from './nostr-uri';
 
 export type Profile = {
@@ -30,15 +29,8 @@ export function toProfile(event: NostrEvent): Profile | null {
 		return null;
 	}
 
-	const quoteIds = metadata.about
-		? extractQuoteIds(metadata.about).filter((id) => !(id in appState.eventsById))
-		: [];
-
-	const contentPubkeys = metadata.about
-		? extractContentPubkeys(metadata.about).filter(
-				(pubkey) => !(pubkey in appState.profilesByPubkey)
-			)
-		: [];
+	const quoteIds = metadata.about ? extractQuoteIds(metadata.about) : [];
+	const contentPubkeys = metadata.about ? extractContentPubkeys(metadata.about) : [];
 
 	const nameTokens = metadata.display_name
 		? parseName(metadata.display_name, event.tags)
